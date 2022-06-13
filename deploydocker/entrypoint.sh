4e62944acfb185e30c0e3f3266619adb706c95f5
Auto-version-incrementer <none@none.com>
2022-06-13 08:48:05 +0000

    k

--- deploydocker/entrypoint.sh
+++ deploydocker/entrypoint.sh
@@ -1,3 +1,3 @@
-poetry run pyright . --lib
+poetry run pyrght . --lib
 poetry run pytest --junitxml=pytest_results_pmd.xml
 poetry run behave tests/features --tags=-skip -f json.cucumber -o tests/behaviour/test-results.json
