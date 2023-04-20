f1d796cc317d63e5e39292817dff377fdc1884f7
Rob Barry <rob.barry@ons.gov.uk>
2023-04-20 16:17:53 +0100

    Removing pull command (moved to csvcubed)

--- src/csvcubedpmd/csvwcli/__init__.py
+++ src/csvcubedpmd/csvwcli/__init__.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 import click
 
-from csvcubedpmd.csvwcli import pull, findwhere
+from csvcubedpmd.csvwcli import findwhere
+
 
 @click.group("csvw")
 def csvw_group():
@@ -9,28 +10,6 @@ def csvw_group():
     Work with CSV-Ws.
     """
     pass
-
-
-@csvw_group.command("pull")
-@click.option(
-    "--out",
-    "-o",
-    help="Output directory in which to place the CSV-W and its relative dependencies",
-    type=click.Path(exists=False, path_type=Path, file_okay=False, dir_okay=True),
-    default="./out",
-    show_default=True,
-    metavar="OUT_DIR",
-)
-@click.argument(
-    "csvw_url",
-    type=click.STRING,
-    metavar="CSVW_METADATA_URL",
-)
-def _pull(out: Path, csvw_url: str):
-    """
-    Pull a CSV-W and all relative dependencies to the local filesystem.
-    """
-    pull.pull(csvw_url, out.absolute())
 
 
 @csvw_group.command("find-where")
